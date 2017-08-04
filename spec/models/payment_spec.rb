2d910dbe55a3f6cff71fac4d9c5581fdaeff8a75
steveallen05 <sallen@amberstyle.ca>
2017-08-04 00:34:24 +0000

    Switching benchmark to utime (more fair)

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   before(:all) { 
-    count = 10_000
+    count = 1_000
     create_list :payment, count, item: 'foo', purchase_price: 9
     create_list :payment, count, item: 'bar', purchase_price: 11
   }
@@ -16,7 +16,7 @@ RSpec.describe Payment, type: :model do
       let(:improved_benchmark) { Benchmark.measure { Payment.improved_get_average } }
       
       it "performs faster by at least 6 orders of magnitude" do
-          expect(improved_benchmark.total).to be <= (original_benchmark.total / 1_000_000)
+          expect(improved_benchmark.utime).to be <= (original_benchmark.utime / 1_000_000)
       end
   end
 end
