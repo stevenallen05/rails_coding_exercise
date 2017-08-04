f60cdb12d856e0bb57120929880f1d323d2509b8
steveallen05 <sallen@amberstyle.ca>
2017-08-04 18:50:21 +0000

    Adding Fizzbuzz rules to spec

--- app/helpers/fizz_buzz_helper.rb
+++ app/helpers/fizz_buzz_helper.rb
@@ -1,5 +1,12 @@
 module FizzBuzzHelper
   def generate(n)
     # Implement a method to satisfy the tests in fizz_buzz_helper_spec.rb
+    
+    # FizzBuzz rules:
+    # For any number evenly divisible by 3, return "Fizz"
+    # For any number evenly divisible by 5, return "Buzz"
+    # For any number evenly divisible by 3 and 5, return "FizzBuzz"
+    # In all other cases, return the number
+    
   end
 end
--- spec/helpers/fizz_buzz_helper_spec.rb
+++ spec/helpers/fizz_buzz_helper_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 describe FizzBuzzHelper do
   describe "FizzBuzz helper" do
+    # FizzBuzz rules:
+    # For any number evenly divisible by 3, return "Fizz"
+    # For any number evenly divisible by 5, return "Buzz"
+    # For any number evenly divisible by 3 and 5, return "FizzBuzz"
+    # In all other cases, return the number
+    
     it { expect(helper.generate(1)).to eq "1" }
     it { expect(helper.generate(2)).to eq "2" }
     it { expect(helper.generate(3)).to eq "Fizz" }
