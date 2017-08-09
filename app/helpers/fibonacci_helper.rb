25189b1c1829272ef9fc1ea059e1f6ca324f9235
steveallen05 <sallen@amberstyle.ca>
2017-08-09 18:36:06 +0000

    Adding fibonacci squence test

--- app/helpers/fibonacci_helper.rb
+++ app/helpers/fibonacci_helper.rb
@@ -2,4 +2,8 @@ module FibonacciHelper
   def generate(n)
     # Implement a method to generate the nth number in the Fibonacci sequence
   end
+  
+  def generate_array(n)
+    # Implement a method to generate the first n numbers in the Fibonacci sequence
+  end
 end
--- spec/helpers/fibonacci_helper_spec.rb
+++ spec/helpers/fibonacci_helper_spec.rb
@@ -6,8 +6,12 @@ describe FibonacciHelper do
     it { expect(helper.generate(1)).to eq 1 }
     it { expect(helper.generate(10)).to eq 55 }
     it { expect(helper.generate(20)).to eq 6_765 }
-    
     ### This one may be slow...
     # it { expect(helper.generate(50)).to eq 12_586_269_025 }
+    
+    let(:first_10_fibonacci_numbers) { [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] }
+    
+    it { expect(helper.generate_array(10)).to eq first_10_fibonacci_numbers }
+    
   end
 end
