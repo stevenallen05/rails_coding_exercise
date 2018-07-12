21da33e7e7afcee7d114553c1c5bbf027196c27e
Kevin Schick <kschick74@gmail.com>
2018-07-12 13:55:48 -0700

    Added tribonacci

--- app/helpers/fibonacci_helper.rb
+++ app/helpers/fibonacci_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Fibonacci numbers are the numbers in the following integer sequence, called the Fibonacci sequence, and characterized
+# by the fact that every number after the first two is the sum of the two preceding ones  1 , 1 , 2 , 3 , 5 , 8 , 13 ,
+# 21 , 34 , 55 , 89 , 144 , ...
 module FibonacciHelper
   def generate_fibonacci(n)
     # Implement a method to generate the nth number in the Fibonacci sequence
